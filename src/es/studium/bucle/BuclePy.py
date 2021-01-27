357db5ea0e8a56510ac2caf5d9a57a9a5e1b9cee
clja1970 <pepecl2020@gmail.com>
2021-01-27 10:34:20 +0100

    Comentario del ejercicio

--- src/es/studium/bucle/BuclePy.py
+++ src/es/studium/bucle/BuclePy.py
@@ -9,5 +9,6 @@ numero=0
 manolo=1
 while numero <10:#los dos puntos dicen que viene la funci�n
     numero+=1
+    #modificando la indentación sale un resultado u otro
     print(numero+manolo)
     
