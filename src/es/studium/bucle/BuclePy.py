6b7c703e4fde7b43c2e0553a3ac7e64beba15fe8
clja1970 <pepecl2020@gmail.com>
2021-01-27 10:31:35 +0100

    Bucle y pruebas con la indentación en la sentencia print

---
+++ src/es/studium/bucle/BuclePy.py
@@ -0,0 +1,13 @@
+#coding: utf-8
+
+'''
+Created on 26 ene. 2021
+
+@author: clja1
+'''
+numero=0
+manolo=1
+while numero <10:#los dos puntos dicen que viene la funci�n
+    numero+=1
+    print(numero+manolo)
+    
